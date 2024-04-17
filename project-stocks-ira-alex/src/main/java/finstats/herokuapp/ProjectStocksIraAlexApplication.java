4e04709d8a8289f3e7cd2dc8b47567035f61bd52
Zamotirina <ir.zamotina@gmail.com>
2024-04-17 15:39:23 +0300

    Master branch

---
+++ project-stocks-ira-alex/src/main/java/finstats/herokuapp/ProjectStocksIraAlexApplication.java
@@ -0,0 +1,13 @@
+package finstats.herokuapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectStocksIraAlexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectStocksIraAlexApplication.class, args);
+	}
+
+}
---
+++ project-stocks-ira-alex/src/test/java/finstats/herokuapp/ProjectStocksIraAlexApplicationTests.java
@@ -0,0 +1,13 @@
+package finstats.herokuapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectStocksIraAlexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
